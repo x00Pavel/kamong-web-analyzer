4165659bfc0f30f23501b6ec164a654c121e32a6
x00Pavel <pavel.yadlouski@gmail.com>
2022-07-23 13:30:47 +0200

    Add sending events to Kafka topic

--- web/app.py
+++ web/app.py
@@ -1,18 +1,52 @@
-from flask import Flask, request, jsonify
+from base64 import encode
+from webbrowser import get
+from flask import Flask, request, jsonify, json
 from flask_restful import Resource, Api
+from kafka import KafkaProducer
 import time
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
+
+KAFKA_BOOTSTRAP_SERVER_NAME = getenv("KAFKA_BOOTSTRAP_SERVER_NAME")
+KAFKA_PORT = getenv("KAFKA_PORT")
+
+assert KAFKA_BOOTSTRAP_SERVER_NAME is not None
+assert KAFKA_PORT is not None
+
+TOPIC_NAME = "quickstart"
+KAFKA_SERVER = f"{KAFKA_BOOTSTRAP_SERVER_NAME}:{KAFKA_PORT}"
+
+producer = KafkaProducer(
+    bootstrap_servers = KAFKA_SERVER,
+    api_version = (0, 11, 15)
+)
 
 
 class Hello(Resource):
 
     def get(self):
-        return jsonify({
+        
+        print(request.json)
+        data = {
             "server_ip": request.host,
             "ip": request.remote_addr,
-            "ts": time.time()})
+            "ts": time.time()}
+        json_paylaod = json.dumps(data)
+        
+        print("Sending to Kafka")
+
+        producer.send(TOPIC_NAME, json_paylaod.encode("utf-8"))
+        producer.flush()
+
+        print("Sent to consumer")
+        print(json_paylaod)
+
+        return jsonify(data)
 
 api.add_resource(Hello, "/")
 
